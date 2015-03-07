c329eaf0e98eaea7388fbf3c3bf18b21aa72a6c9
Sergey Romanov <xxsmotur@gmail.com>
2015-03-07 21:34:13 +0500

    Fix bugs, append events for triggers

--- dirwatcher.go
+++ dirwatcher.go
@@ -16,13 +16,25 @@ type
 	taskfunc func (string)
 )
 
+/* Struct for define, when trigger happens. */
+type Event struct {
+	Changing bool
+	Append bool
+	Remove bool
+}
+
+type EventData struct {
+	 trigger taskfunc
+	 event Event
+}
+
 type DirWatcher struct{
 	dirs [] string
 	changes map[string]time.Time
 	dirchanges map[string]time.Time
-	triggers []taskfunc
+	triggers []EventData
 	mutex sync.Mutex
-	isstarted bool
+	isstarted []bool
 	//If Notify in Options is true
 	notshowinfo bool
 	tick time.Duration
@@ -31,13 +43,18 @@ type DirWatcher struct{
 	runstat bool
 }
 
+
 type Options struct {
 	//Show messages about append, modify etc
 	Notshowinfo bool
 
 	//Show statistics every n seconds
 	Showstat uint
+
+	//Show initial append files
+	Showinitappend bool
 }
+
 
 //Statistics
 type Stat struct {
@@ -50,8 +67,9 @@ func Init(opt ...Options)*DirWatcher {
 	dirwatch.dirs = []string{}
 	dirwatch.changes = make(map[string]time.Time)
 	dirwatch.dirchanges = make(map[string]time.Time)
-	dirwatch.triggers = []taskfunc{}
-	dirwatch.isstarted = false
+	//dirwatch.triggers = []taskfunc{}
+	dirwatch.triggers = []EventData {}
+	dirwatch.isstarted = [] bool {}
 	if len(opt) > 0 && opt[0].Notshowinfo == true {
 		dirwatch.notshowinfo = true
 	}
@@ -59,6 +77,10 @@ func Init(opt ...Options)*DirWatcher {
 	if len(opt) > 0 && opt[0].Showstat > 0{
 		dirwatch.runstat = true
 		dirwatch.tick = (8*time.Second)
+	}
+
+	if len(opt) > 0 && opt[0].Showinitappend == true {
+		fmt.Println(opt)
 	}
 	return dirwatch
 }
@@ -69,23 +91,18 @@ func Init(opt ...Options)*DirWatcher {
 */
 func (d*DirWatcher) AddDir(path string){
 	d.dirs = append(d.dirs, path)
+	d.isstarted = append(d.isstarted, false)
+	//d.isstarted[0] = false
 }
 
-
-/*
-	Append new Task
-*/
-func (*DirWatcher) AddTask(path string){
-
-}
 
 /*
 	Append new traigger. (For example, do it something, when has changed specific file)
 
 */
 
-func (d*DirWatcher) AddTrigger(somefunc taskfunc){
-	d.triggers = append(d.triggers, somefunc)
+func (d*DirWatcher) AddTrigger(somefunc taskfunc,  event ... Event){
+	d.triggers = append(d.triggers, EventData {somefunc, event[0]})
 }
 
 /*
@@ -97,8 +114,8 @@ func (d*DirWatcher) Run(){
 	}
 	fmt.Println("Start dirwatcher")
 	for {
-		for _ ,dir:= range d.dirs {
-			d.getAllFromDir(dir)
+		for i ,dir:= range d.dirs {
+			d.getAllFromDir(dir, i)
 		}
 		time.Sleep(100 * time.Millisecond)
 	}
@@ -108,32 +125,36 @@ func CreateDir (path string){
 	os.Mkdir(path, 0777)
 }
 
-func (d*DirWatcher) getAllFromDir(path string){
+func (d*DirWatcher) getAllFromDir(path string, i int){
 	files, err := ioutil.ReadDir(path)
 	if err != nil{
 		log.Fatal("Cant find target dir")
 	}
     for _, f := range files {
+    		fullpath := path + "/" + f.Name()
             switch{
             case f.IsDir():
-            	d.dirchanges[f.Name()] = f.ModTime()
+            	d.dirchanges[fullpath] = f.ModTime()
             default:
             	name := f.Name()
             	info := f.ModTime()
-            	item, ok := d.changes[name]
+            	item, ok := d.changes[fullpath]
+            	//Fullpath for this file
             	if !ok{
-            		if d.isstarted{
+            		if d.isstarted[i]{
             			d.mutex.Lock()
             			d.stat.total_append += 1
             			d.mutex.Unlock()
-            			d.showInfo("This file was append: "+ name)
+            			d.checkTriggers(name, Event {Append: true})
+            			d.showInfo("This file was append: "+ fullpath)
             		}
-            		d.changes[name] = info
+            		d.changes[fullpath] = info
             	} else{
             		if item != info{
-            			d.showInfo("This file is changed: "+ name)
-            			d.changes[name] = info
-            			d.checkTriggers(name)
+            			d.showInfo("This file is changed: "+ fullpath)
+            			d.changes[fullpath] = info
+            			d.checkTriggers(name, Event {Changing: true})
+            			//d.checkTriggers(fullpath)
             			d.mutex.Lock()
             			d.stat.total_changed += 1
             			d.mutex.Unlock()
@@ -142,7 +163,7 @@ func (d*DirWatcher) getAllFromDir(path string){
 
             }
     }
-    d.isstarted = true
+    d.isstarted[i] = true //; Note: Works only for one dir
 }
 
 /*
@@ -154,10 +175,15 @@ func (d*DirWatcher) showInfo(msg string){
 	}
 }
 
-func (d*DirWatcher) checkTriggers(path string){
-	for _, trigger := range d.triggers{
+func (d*DirWatcher) checkTriggers(path string, typedata Event){
+	for _, value := range d.triggers{
 		d.mutex.Lock()
-		go trigger(path)
+		if value.event.Changing == true && typedata.Changing == true{
+			go value.trigger(path)
+		}
+		if value.event.Remove == true && typedata.Remove == true {
+			go value.trigger(path)
+		}
 		d.mutex.Unlock()
 	}
 }
