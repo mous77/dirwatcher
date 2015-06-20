3480eed55c68e94872093a7baac44ec83d51cc5f
Sergey Romanov <xxsmotur@gmail.com>
2015-06-20 23:54:00 +0500

    Puppet for rest server

--- dirwatcher.go
+++ dirwatcher.go
@@ -1,59 +1,59 @@
 package dirwatcher
-import
-(
+
+import (
 	"fmt"
-	"os"
+	"github.com/ant0ine/go-json-rest/rest"
+	"github.com/op/go-logging"
+	"io"
 	"io/ioutil"
+	"os"
+	"path"
 	"sync"
 	"time"
-	"github.com/op/go-logging"
-	"path"
-	"io"
+	"net/http"
 )
 
 var log = logging.MustGetLogger("lightstore_log")
 
 //TODO: Append http for view
-type 
-(
-	taskfunc func (string, *DirWatcher)
+type (
+	taskfunc func(string, *DirWatcher)
 )
 
 /* Struct for define, when trigger happens. */
 type Event struct {
 	Changing bool
-	Append bool
-	Remove bool
+	Append   bool
+	Remove   bool
 }
 
 type EventData struct {
-	 trigger taskfunc
-	 event Event
+	trigger taskfunc
+	event   Event
 }
 
-type DirWatcher struct{
+type DirWatcher struct {
 	//All directories for watching
-	dirs [] string
+	dirs []string
 	//All files durning watching
 	changes map[string]time.Time
 	//All directories dyrning watching
 	dirchanges map[string]time.Time
 	exceptions map[string]bool
-	triggers []EventData
-	mutex sync.Mutex
-	isstarted []bool
+	triggers   []EventData
+	mutex      sync.Mutex
+	isstarted  []bool
 	//If Notify in Options is true
 	notshowinfo bool
-	tick time.Duration
+	tick        time.Duration
 	//Statistics
-	stat Stat
-	runstat bool
+	stat        Stat
+	runstat     bool
 	loopstarted bool
-	file *os.File
+	file        *os.File
 	//directory for backup files
 	backupdir string
 }
-
 
 type Options struct {
 	//Show messages about append, modify etc
@@ -72,44 +72,43 @@ type Options struct {
 	Backup string
 }
 
-
 //Statistics
 type Stat struct {
-	total_append uint
+	total_append  uint
 	total_changed uint
 }
 
-func Init(opt ...Options)*DirWatcher {
+func Init(opt ...Options) *DirWatcher {
 	dirwatch := new(DirWatcher)
 	dirwatch.dirs = []string{}
 	dirwatch.exceptions = make(map[string]bool)
 	dirwatch.changes = make(map[string]time.Time)
 	dirwatch.dirchanges = make(map[string]time.Time)
 	//dirwatch.triggers = []taskfunc{}
-	dirwatch.triggers = []EventData {}
-	dirwatch.isstarted = [] bool {}
+	dirwatch.triggers = []EventData{}
+	dirwatch.isstarted = []bool{}
 	if len(opt) > 0 && opt[0].Notshowinfo == true {
 		dirwatch.notshowinfo = true
 	}
 
 	/*
-	Show statistics(Stat) after n seconds
+		Show statistics(Stat) after n seconds
 
-	Statistics will be in this format (by default):
-	2015-03-22 21:44:17.405990879 +0500 YEKT
-	Total append:  1
-	Total changed:  1
+		Statistics will be in this format (by default):
+		2015-03-22 21:44:17.405990879 +0500 YEKT
+		Total append:  1
+		Total changed:  1
 	*/
-	if len(opt) > 0 && opt[0].Showstat > 0{
+	if len(opt) > 0 && opt[0].Showstat > 0 {
 		dirwatch.runstat = true
-		dirwatch.tick = (8*time.Second)
+		dirwatch.tick = (8 * time.Second)
 	}
 
 	if len(opt) > 0 && opt[0].Showinitappend == true {
 		fmt.Println(opt)
 	}
 
-	if len(opt) > 0 && opt[0].Logfile != ""{
+	if len(opt) > 0 && opt[0].Logfile != "" {
 		Logfile := opt[0].Logfile
 		f, err := os.Create(Logfile)
 		if err == nil {
@@ -131,11 +130,11 @@ func Init(opt ...Options)*DirWatcher {
 /*
 	Copy files to backup dir before starting of watching
 */
-func (d*DirWatcher) copyToBackup(){
+func (d *DirWatcher) copyToBackup() {
 	fmt.Println("Copt files to backup dir(**Test version**)")
 	for _, dirname := range d.dirs {
 		files, err := ioutil.ReadDir(dirname)
-		if err != nil{
+		if err != nil {
 			log.Fatal("Cant find target dir")
 		}
 
@@ -153,17 +152,17 @@ func (d*DirWatcher) copyToBackup(){
 			if errcopy != nil {
 				panic(err)
 			}
-		}	
+		}
 	}
 }
 
-/* 
+/*
 	Append new directory for watching
 */
-func (d*DirWatcher) AddDir(path string){
-	for _, name := range d.dirs{
-		if(path == name) {
-			return 
+func (d *DirWatcher) AddDir(path string) {
+	for _, name := range d.dirs {
+		if path == name {
+			return
 		}
 	}
 	d.dirs = append(d.dirs, path)
@@ -171,22 +170,21 @@ func (d*DirWatcher) AddDir(path string){
 	//d.isstarted[0] = false
 }
 
-
 /*
 	Append new traigger. (For example, do it something, when has changed specific file)
 
 */
 
-func (d*DirWatcher) AddTrigger(somefunc taskfunc,  event ... Event){
+func (d *DirWatcher) AddTrigger(somefunc taskfunc, event ...Event) {
 	for _, funcaddr := range d.triggers {
-		if(&somefunc == &funcaddr.trigger){
-			return 
+		if &somefunc == &funcaddr.trigger {
+			return
 		}
 	}
-	d.triggers = append(d.triggers, EventData {somefunc, event[0]})
+	d.triggers = append(d.triggers, EventData{somefunc, event[0]})
 }
 
-func (d*DirWatcher) showDirs(){
+func (d *DirWatcher) showDirs() {
 	fmt.Println("Watching directories:")
 	for _, dir := range d.dirs {
 		fmt.Println(dir)
@@ -194,88 +192,109 @@ func (d*DirWatcher) showDirs(){
 }
 
 //Run is start working of dirwatcher
-func (d*DirWatcher) Run(){
+func (d *DirWatcher) Run() {
 	if d.runstat == true {
 		go d.tickEvery()
 	}
 
-	if(len(d.dirs) == 0) {
+	if len(d.dirs) == 0 {
 		panic("Not found directory for watching")
 	}
 
-	if(d.backupdir != "") {
+	if d.backupdir != "" {
 		d.copyToBackup()
 	}
-	
+
 	d.showDirs()
 	fmt.Println("Start dirwatcher")
 	d.loopstarted = true
+	go d.RunServer()
 	for {
-		for i ,dir:= range d.dirs {
+		for i, dir := range d.dirs {
 			d.getAllFromDir(dir, i)
 		}
-		if(!d.loopstarted) {
+		if !d.loopstarted {
 			break
 		}
 		time.Sleep(100 * time.Millisecond)
 	}
 }
 
+
+//RunServer starts rest server
+func (d *DirWatcher) RunServer() {
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	router, err := rest.MakeRouter(
+		&rest.Route{"POST", "/dirwatcher", func(w rest.ResponseWriter, r *rest.Request){
+			w.WriteJson("Element was append")
+		}},
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	api.SetApp(router)
+	addr:= "localhost"
+	port := 8080
+	http.ListenAndServe(fmt.Sprintf("%s:%d", addr, port), api.MakeHandler())
+}
+
 //Stop the main loop
-func (d*DirWatcher) Stop (){
+func (d *DirWatcher) Stop() {
 	d.loopstarted = false
 }
 
-func CreateDir (path string){
+func CreateDir(path string) {
 	os.Mkdir(path, 0777)
 }
 
-func (d*DirWatcher) getAllFromDir(path string, i int){
+func (d *DirWatcher) getAllFromDir(path string, i int) {
 	files, err := ioutil.ReadDir(path)
-	if err != nil{
+	if err != nil {
 		log.Fatal("Cant find target dir")
 	}
-    for _, f := range files {
-    		fullpath := path + "/" + f.Name()
-            switch{
-            case f.IsDir():
-            	d.dirchanges[fullpath] = f.ModTime()
-            default:
-            	name := f.Name()
-            	info := f.ModTime()
-            	item, ok := d.changes[fullpath]
-            	//Fullpath for this file
-            	if !ok{
-            		if d.isstarted[i]{
-            			d.mutex.Lock()
-            			d.stat.total_append += 1
-            			d.mutex.Unlock()
-            			d.checkTriggers(name, Event {Append: true})
-            			d.showInfo("This file was append: "+ fullpath)
-            		}
-            		d.changes[fullpath] = info
-            	} else{
-            		_, errcon := d.exceptions[fullpath]
-            		if item != info && !errcon{
-            			d.showInfo("This file is changed: "+ fullpath)
-            			d.changes[fullpath] = info
-            			d.checkTriggers(name, Event {Changing: true})
-            			//d.checkTriggers(fullpath)
-            			d.mutex.Lock()
-            			d.stat.total_changed += 1
-            			d.mutex.Unlock()
-            		}
-            	}
+	for _, f := range files {
+		fullpath := path + "/" + f.Name()
+		switch {
+		case f.IsDir():
+			d.dirchanges[fullpath] = f.ModTime()
+		default:
+			name := f.Name()
+			info := f.ModTime()
+			item, ok := d.changes[fullpath]
+			//Fullpath for this file
+			if !ok {
+				if d.isstarted[i] {
+					d.mutex.Lock()
+					d.stat.total_append += 1
+					d.mutex.Unlock()
+					d.checkTriggers(name, Event{Append: true})
+					d.showInfo("This file was append: " + fullpath)
+				}
+				d.changes[fullpath] = info
+			} else {
+				_, errcon := d.exceptions[fullpath]
+				if item != info && !errcon {
+					d.showInfo("This file is changed: " + fullpath)
+					d.changes[fullpath] = info
+					d.checkTriggers(name, Event{Changing: true})
+					//d.checkTriggers(fullpath)
+					d.mutex.Lock()
+					d.stat.total_changed += 1
+					d.mutex.Unlock()
+				}
+			}
 
-            }
-    }
-    d.isstarted[i] = true //; Note: Works only for one dir
+		}
+	}
+	d.isstarted[i] = true //; Note: Works only for one dir
 }
 
 /*
 	We can only manage "system" messages(append, remove...). It not contain triggers
 */
-func (d*DirWatcher) showInfo(msg string){
+func (d *DirWatcher) showInfo(msg string) {
 	if d.notshowinfo != true {
 		fmt.Println(msg)
 	}
@@ -288,10 +307,10 @@ func (d*DirWatcher) showInfo(msg string){
 	}
 }
 
-func (d*DirWatcher) checkTriggers(path string, typedata Event){
-	for _, value := range d.triggers{
+func (d *DirWatcher) checkTriggers(path string, typedata Event) {
+	for _, value := range d.triggers {
 		d.mutex.Lock()
-		if value.event.Changing == true && typedata.Changing == true{
+		if value.event.Changing == true && typedata.Changing == true {
 			go value.trigger(path, d)
 		}
 		if value.event.Remove == true && typedata.Remove == true {
@@ -301,9 +320,9 @@ func (d*DirWatcher) checkTriggers(path string, typedata Event){
 	}
 }
 
-func (d*DirWatcher) tickEvery(){
-	for i := range time.Tick(d.tick){
-		go func(){
+func (d *DirWatcher) tickEvery() {
+	for i := range time.Tick(d.tick) {
+		go func() {
 			d.mutex.Lock()
 			fmt.Println(i)
 			fmt.Println("Total append: ", d.stat.total_append)
