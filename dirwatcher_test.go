61c07f7dd6e3d8f295df996b812a93c93a5a7ed3
Sergey Romanov <xxsmotur@gmail.com>
2015-11-14 20:13:36 +0500

    Update

--- dirwatcher_test.go
+++ dirwatcher_test.go
@@ -1,18 +1,18 @@
 package dirwatcher
 
 import (
-  "testing"
-  //"sync"
-  "os"
-  "time"
-)
-const (
-	dirdata = "../testdata/"
+	"testing"
+	//"sync"
+	"os"
+	"time"
 )
 
+const (
+	dirdata = "./testdata/"
+)
 
 func createFile(path string) error {
-	f, err := os.Open(path)
+	f, err := os.Create(path)
 	defer f.Close()
 	if err != nil {
 		return err
@@ -22,7 +22,7 @@ func createFile(path string) error {
 }
 
 func modifyFile(path string) error {
-	f, err := os.OpenFile("foo.txt", os.O_RDWR|os.O_APPEND, 0660)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_APPEND, 0660)
 	defer f.Close()
 	if err != nil {
 		return err
@@ -31,27 +31,43 @@ func modifyFile(path string) error {
 	return nil
 }
 
-
 func TestWatchingFiles(t *testing.T) {
 	path := dirdata + "watch1"
 	err := createFile(path)
 	if err != nil {
-		t.Error("%v", err)
+		t.Errorf("%v", err)
 	}
 	defer os.Remove(path)
 
-
 	watcher := Init()
-	trigged := false
-	watcher.AddFile(path, func(item string, d *DirWatcher){
-		trigged = true
+	complete := false
+	watcher.AddFile(path, func(item string, d *DirWatcher) {
+		if path == item {
+			complete = true
+		}
 	})
 	boo := make(chan bool, 1)
 	go func() {
 		watcher.Run()
 	}()
 	go func() {
-		time.Sleep(time.Second)
-        boo <- true
+		time.Sleep(time.Second * 2)
+		boo <- true
 	}()
+
+	modifyFile(path)
+	tickChan := time.NewTicker(time.Millisecond * 400).C
+	for {
+		select {
+		case <-tickChan:
+			if complete {
+				return
+			}
+		case <-boo:
+			{
+				t.Errorf("Fail %d", complete)
+				return
+			}
+		}
+	}
 }
